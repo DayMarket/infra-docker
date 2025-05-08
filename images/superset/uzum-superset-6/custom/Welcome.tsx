d399c30505187b8b8cd69876bae059824f95b2f0
paulDashkevich <info@jetrom.by>
2025-05-08 21:10:36 +0300

    fix: upd patch

--- images/superset/uzum-superset-6/custom/Welcome.tsx
+++ images/superset/uzum-superset-6/custom/Welcome.tsx
@@ -1,102 +1,128 @@
 import React, { useEffect, useState } from 'react';
-import { t } from '@superset-ui/core';
-import { useSelector } from 'react-redux';
-import { UserWithPermissionsAndRoles } from 'src/types/bootstrapTypes';
-import { WelcomeDashboard, WelcomeChart } from './types';
-import { EmptyStateBig } from 'src/components/EmptyState';
-import { CardGrid } from 'src/components/CardGrid';
-import DashboardCard from 'src/custom/DashboardCard';
-import ChartCard from 'src/custom/ChartCard';
-import { getRecentDashboards, getRecentCharts } from './utils';
+import rison from 'rison';
+import { t, SupersetClient } from '@superset-ui/core';
+import { Dashboard, Chart, SavedQueryObject } from 'src/types';
+import ListViewCard from 'src/components/ListViewCard';
+import { createErrorHandler } from 'src/views/CRUD/utils';
+import { WelcomeContainer, CardContainer, CardStyles } from './styles';
+
+const PAGE_SIZE = 5;
 
 export default function Welcome() {
-  const user = useSelector<any, UserWithPermissionsAndRoles>(
-    state => state.user,
-  );
-  const [dashboards, setDashboards] = useState<WelcomeDashboard[]>([]);
-  const [charts, setCharts] = useState<WelcomeChart[]>([]);
-  const [loading, setLoading] = useState(true);
+  const [recentDashboards, setRecentDashboards] = useState<Dashboard[]>([]);
+  const [userDashboards, setUserDashboards] = useState<Dashboard[]>([]);
+  const [userCharts, setUserCharts] = useState<Chart[]>([]);
+  const [userQueries, setUserQueries] = useState<SavedQueryObject[]>([]);
+
+  const fetchData = () => {
+    SupersetClient.get({
+      endpoint: `/api/v1/dashboard/?q=${rison.encode({
+        page_size: PAGE_SIZE,
+        order_column: 'changed_on', // ⬅ заменено для ускорения
+        order_direction: 'desc',
+      })}`,
+    })
+      .then(({ json }) => {
+        setRecentDashboards(json.result);
+      })
+      .catch(createErrorHandler(t('There was an error fetching recent dashboards')));
+
+    SupersetClient.get({
+      endpoint: `/api/v1/dashboard/?q=${rison.encode({
+        page_size: PAGE_SIZE,
+        order_column: 'changed_on',
+        order_direction: 'desc',
+        filters: [{ col: 'owners', opr: 'rel_m_m', value: 'self' }],
+      })}`,
+    })
+      .then(({ json }) => {
+        setUserDashboards(json.result);
+      })
+      .catch(createErrorHandler(t('There was an error fetching your dashboards')));
+
+    SupersetClient.get({
+      endpoint: `/api/v1/chart/?q=${rison.encode({
+        page_size: PAGE_SIZE,
+        order_column: 'changed_on',
+        order_direction: 'desc',
+        filters: [{ col: 'owners', opr: 'rel_m_m', value: 'self' }],
+      })}`,
+    })
+      .then(({ json }) => {
+        setUserCharts(json.result);
+      })
+      .catch(createErrorHandler(t('There was an error fetching your charts')));
+
+    SupersetClient.get({
+      endpoint: `/api/v1/saved_query/?q=${rison.encode({
+        page_size: PAGE_SIZE,
+        order_column: 'changed_on',
+        order_direction: 'desc',
+        filters: [{ col: 'created_by', opr: 'rel_o_m', value: 'self' }],
+      })}`,
+    })
+      .then(({ json }) => {
+        setUserQueries(json.result);
+      })
+      .catch(createErrorHandler(t('There was an error fetching your saved queries')));
+  };
 
   useEffect(() => {
-    let isMounted = true;
-    async function fetchData() {
-      setLoading(true);
-      try {
-        const [dashboardsResponse, chartsResponse] = await Promise.all([
-          getRecentDashboards(),
-          getRecentCharts(),
-        ]);
-        if (isMounted) {
-          setDashboards(dashboardsResponse);
-          setCharts(chartsResponse);
-        }
-      } finally {
-        if (isMounted) {
-          setLoading(false);
-        }
-      }
-    }
-
     fetchData();
-    return () => {
-      isMounted = false;
-    };
   }, []);
 
   return (
-    <>
-      <h2>{t('Dashboards')}</h2>
-      {dashboards.length === 0 && !loading ? (
-        <EmptyStateBig
-          title={t('No dashboards yet')}
-          description={t('You haven’t viewed any dashboards yet.')}
-        />
-      ) : (
-        <CardGrid
-          cards={dashboards.map(dashboard => (
-            <DashboardCard
-              key={`dashboard-${dashboard.id}`}
-              dashboard={dashboard}
-              showThumbnails
-              userId={user?.userId}
-              hasPerm={() => true}
-              bulkSelectEnabled={false}
-              loading={false}
-              saveFavoriteStatus={() => {}}
-              favoriteStatus={false}
-              handleBulkDashboardExport={() => {}}
-              onDelete={() => {}}
-            />
-          ))}
-          showLoading={loading}
-        />
-      )}
+    <WelcomeContainer>
+      <h2>{t('Recently modified dashboards')}</h2>
+      <CardContainer>
+        {recentDashboards.map(dash => (
+          <ListViewCard
+            key={`recent-dash-${dash.id}`}
+            title={dash.dashboard_title}
+            url={`/superset/dashboard/${dash.id}/`}
+            imgURL={dash.thumbnail_url || undefined}
+            description={dash.description}
+          />
+        ))}
+      </CardContainer>
 
-      <h2 style={{ marginTop: '3rem' }}>{t('Charts')}</h2>
-      {charts.length === 0 && !loading ? (
-        <EmptyStateBig
-          title={t('No charts yet')}
-          description={t('You haven’t viewed any charts yet.')}
-        />
-      ) : (
-        <CardGrid
-          cards={charts.map(chart => (
-            <ChartCard
-              key={`chart-${chart.id}`}
-              chart={chart}
-              showThumbnails
-              userId={user?.userId}
-              hasPerm={() => true}
-              bulkSelectEnabled={false}
-              favoriteStatus={false}
-              saveFavoriteStatus={() => {}}
-              handleBulkChartExport={() => {}}
-              onDelete={() => {}}
-            />
-          ))}
-          showLoading={loading}
-        />
-      )}
-    </>
+      <h2>{t('Your dashboards')}</h2>
+      <CardContainer>
+        {userDashboards.map(dash => (
+          <ListViewCard
+            key={`user-dash-${dash.id}`}
+            title={dash.dashboard_title}
+            url={`/superset/dashboard/${dash.id}/`}
+            imgURL={dash.thumbnail_url || undefined}
+            description={dash.description}
+          />
+        ))}
+      </CardContainer>
+
+      <h2>{t('Your charts')}</h2>
+      <CardContainer>
+        {userCharts.map(chart => (
+          <ListViewCard
+            key={`user-chart-${chart.id}`}
+            title={chart.slice_name}
+            url={`/explore/?slice_id=${chart.id}`}
+            imgURL={chart.thumbnail_url || undefined}
+            description={chart.description}
+          />
+        ))}
+      </CardContainer>
+
+      <h2>{t('Your saved queries')}</h2>
+      <CardContainer>
+        {userQueries.map(query => (
+          <ListViewCard
+            key={`user-query-${query.id}`}
+            title={query.label}
+            url={`/sql_lab/?savedQueryId=${query.id}`}
+            description={query.sql}
+          />
+        ))}
+      </CardContainer>
+    </WelcomeContainer>
   );
 }
