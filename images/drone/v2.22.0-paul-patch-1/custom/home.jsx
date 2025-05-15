728c2aa127881514adda06d5b312ae761445d5a1
paulDashkevich <info@jetrom.by>
2025-05-15 22:58:48 +0300

    fix: upd home.jsx

--- images/drone/v2.22.0-paul-patch-1/custom/home.jsx
+++ images/drone/v2.22.0-paul-patch-1/custom/home.jsx
@@ -8,13 +8,10 @@ import React, {
 } from 'react';
 import Repos from 'components/pages/home/repos';
 import ReposRecent from 'components/pages/home/repos-recent';
-import Button from 'components/shared/button';
 import Input from 'components/shared/form/input';
-import Select from 'components/shared/form/select';
-import Switch from 'components/shared/switch';
 import ZeroState from 'components/shared/zero-state';
 import { AppContext } from 'context';
-import { useLocalStorage, useCustomTitle, useToast } from 'hooks';
+import { useCustomTitle, useToast } from 'hooks';
 import { useStore } from 'hooks/store';
 import { useViewer, useSyncAccount } from 'hooks/swr';
 import { byBuildCreatedAtDesc } from 'utils';
@@ -27,18 +24,20 @@ const REPOS_CHUNK_SIZE = 20;
 
 export default function Home() {
   const [context, setContext] = useContext(AppContext);
-  const [isActiveOnly] = useState(true);
   const [shouldStartSync, setShouldStartSync] = useState(context.isAccSyncing);
   const { showError } = useToast();
   const { hasSyncReqFiredOff, isError: syncError } = useSyncAccount(shouldStartSync);
-  const { isSynced, isSyncing, isError: viewerError } = useViewer({ withPolling: hasSyncReqFiredOff });
+  const { isSynced, isSyncing, isError: viewerError } = useViewer({
+    withPolling: hasSyncReqFiredOff,
+  });
 
-  const {
+  const { 
     repos,
     error,
     reload,
     reloadOnce
   } = useStore();
+  
   const data = repos ? Object.values(repos) : undefined;
   const isLoading = !data && !error;
 
@@ -49,13 +48,18 @@ export default function Home() {
   useCustomTitle();
 
   const filtered = useMemo(
-    () => data?.filter(repo => (isActiveOnly ? repo.active : true))
-      .filter(repo => repo.slug.includes(filter)) ?? [],
-    [data, isActiveOnly, filter],
+    () =>
+      data?.filter(repo => repo.slug.includes(filter)) ?? [],
+    [data, filter],
   );
 
   const recent = useMemo(
-    () => data?.slice(0).sort(byBuildCreatedAtDesc).filter(repo => repo.build).slice(0, 6) ?? [],
+    () =>
+      data
+        ?.slice(0)
+        .sort(byBuildCreatedAtDesc)
+        .filter(repo => repo.build)
+        .slice(0, 6) ?? [],
     [data],
   );
 
@@ -63,7 +67,10 @@ export default function Home() {
     if (syncError || viewerError) {
       setContext({ ...context, isAccSyncing: false });
       showError('Sync error has occurred, please, try again');
-      console.error('Sync error:', syncError?.message || viewerError?.message); // eslint-disable-line no-console
+      console.error(
+        'Sync error:',
+        syncError?.message || viewerError?.message,
+      ); // eslint-disable-line no-console
     }
   }, [syncError, viewerError, showError, context, setContext]);
 
@@ -83,7 +90,7 @@ export default function Home() {
     setItemsToShow(prev => prev + REPOS_CHUNK_SIZE);
   };
 
-  const handleFilter = (e) => setFilter(e.target.value.trim());
+  const handleFilter = e => setFilter(e.target.value.trim());
 
   return (
     <>
@@ -104,20 +111,15 @@ export default function Home() {
       <section className={cx('wrapper')}>
         {!!recent.length && (
           <>
-            <h2 className={cx('section-title', 'section-title-recent')}>Recent Activity</h2>
+            <h2 className={cx('section-title', 'section-title-recent')}>
+              Recent Activity
+            </h2>
             <ReposRecent repos={recent} />
           </>
         )}
         <div className={cx('subheader')}>
           <h2 className={cx('section-title')}>Repositories</h2>
           <div className={cx('actions')}>
-            <Switch
-              id="active-switch"
-              checked={isActiveOnly}
-              onChange={() => {}}
-            >
-              Active Only
-            </Switch>
             <Input
               placeholder="Filter …"
               icon="search"
