8b41aa1e3838c2f157e3d7c8f484257bbc5890ba
paulDashkevich <info@jetrom.by>
2025-05-17 01:04:52 +0300

    fix: webs

--- images/drone/v2.22.0-paul-patch-3/custom/web.go
+++ images/drone/v2.22.0-paul-patch-3/custom/web.go
@@ -1,59 +1,137 @@
+// Copyright 2019 Drone IO, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package web
 
 import (
-	"bytes"
-	"crypto/md5"
-	"fmt"
 	"net/http"
-	"os"
-	"time"
 
 	"github.com/drone/drone/core"
+	"github.com/drone/drone/handler/web/link"
+	"github.com/drone/drone/logger"
+	"github.com/drone/go-login/login"
+	"github.com/drone/go-scm/scm"
+
+	chiprometheus "github.com/766b/chi-prometheus"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"github.com/unrolled/secure"
 )
 
-func HandleIndex(host string, session core.Session, license core.LicenseService) http.HandlerFunc {
-	return func(rw http.ResponseWriter, r *http.Request) {
-		user, _ := session.Get(r)
-		if user == nil && r.URL.Path == "/" {
-			http.Redirect(rw, r, "/welcome", 303)
-			return
-		}
+// Server is a http.Handler which exposes drone functionality over HTTP.
+type Server struct {
+	Admitter  core.AdmissionService
+	Builds    core.BuildStore
+	Client    *scm.Client
+	Hooks     core.HookParser
+	License   *core.License
+	Licenses  core.LicenseService
+	Linker    core.Linker
+	Login     login.Middleware
+	Repos     core.RepositoryStore
+	Session   core.Session
+	Syncer    core.Syncer
+	Triggerer core.Triggerer
+	Users     core.UserStore
+	Userz     core.UserService
+	Webhook   core.WebhookSender
+	Options   secure.Options
+	Host      string
+}
 
-		out, err := os.ReadFile("./static/index.html")
-		if err != nil {
-			http.Error(rw, "UI not found", http.StatusInternalServerError)
-			return
-		}
-
-		ctx := r.Context()
-		if ok, _ := license.Exceeded(ctx); ok {
-			out = bytes.Replace(out, head, exceeded, -1)
-		} else if license.Expired(ctx) {
-			out = bytes.Replace(out, head, expired, -1)
-		}
-
-		rw.Header().Set("Content-Type", "text/html; charset=UTF-8")
-		rw.Write(out)
+// New returns a new http server.
+func New(
+	admitter core.AdmissionService,
+	builds core.BuildStore,
+	client *scm.Client,
+	hooks core.HookParser,
+	license *core.License,
+	licenses core.LicenseService,
+	linker core.Linker,
+	login login.Middleware,
+	repos core.RepositoryStore,
+	session core.Session,
+	syncer core.Syncer,
+	triggerer core.Triggerer,
+	users core.UserStore,
+	userz core.UserService,
+	webhook core.WebhookSender,
+	options secure.Options,
+	system *core.System,
+) Server {
+	return Server{
+		Admitter:  admitter,
+		Builds:    builds,
+		Client:    client,
+		Hooks:     hooks,
+		License:   license,
+		Licenses:  licenses,
+		Linker:    linker,
+		Login:     login,
+		Repos:     repos,
+		Session:   session,
+		Syncer:    syncer,
+		Triggerer: triggerer,
+		Users:     users,
+		Userz:     userz,
+		Webhook:   webhook,
+		Options:   options,
+		Host:      system.Host,
 	}
 }
 
-var (
-	head     = []byte(`<head>`)
-	expired  = []byte(`<head><script>window.LICENSE_EXPIRED=true</script>`)
-	exceeded = []byte(`<head><script>window.LICENSE_LIMIT_EXCEEDED=true</script>`)
-)
+// Handler returns an http.Handler
+func (s Server) Handler() http.Handler {
+	r := chi.NewRouter()
+	m := chiprometheus.NewMiddleware("web")
+	r.Use(m)
+	r.Use(middleware.Recoverer)
+	r.Use(middleware.NoCache)
+	r.Use(logger.Middleware)
+	r.Use(middleware.StripSlashes)
 
-func setupCache(h http.Handler) http.Handler {
-	data := []byte(time.Now().String())
-	etag := fmt.Sprintf("%x", md5.Sum(data))
+	sec := secure.New(s.Options)
+	r.Use(sec.Handler)
 
-	return http.HandlerFunc(
-		func(w http.ResponseWriter, r *http.Request) {
-			w.Header().Set("Cache-Control", "public, max-age=31536000")
-			w.Header().Del("Expires")
-			w.Header().Del("Pragma")
-			w.Header().Set("ETag", etag)
-			h.ServeHTTP(w, r)
-		},
+	r.Route("/hook", func(r chi.Router) {
+		r.Post("/", HandleHook(s.Repos, s.Builds, s.Triggerer, s.Hooks))
+	})
+
+	r.Get("/link/{namespace}/{name}/tree/*", link.HandleTree(s.Linker))
+	r.Get("/link/{namespace}/{name}/src/*", link.HandleTree(s.Linker))
+	r.Get("/link/{namespace}/{name}/commit/{commit}", link.HandleCommit(s.Linker))
+	r.Get("/version", HandleVersion)
+	r.Get("/varz", HandleVarz(s.Client, s.License))
+
+	r.Handle("/login",
+		s.Login.Handler(
+			http.HandlerFunc(
+				HandleLogin(
+					s.Users,
+					s.Userz,
+					s.Syncer,
+					s.Session,
+					s.Admitter,
+					s.Webhook,
+				),
+			),
+		),
 	)
+	r.Get("/logout", HandleLogout())
+	r.Post("/logout", HandleLogout())
+
+	r.NotFound(HandleIndex(s.Host, s.Session, s.Licenses))
+
+	return r
 }
