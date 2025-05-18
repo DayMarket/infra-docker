d7c834eeb59a7127355302dd6dbc4e9fbf0cc767
paulDashkevich <info@jetrom.by>
2025-05-19 01:33:04 +0300

    fix: use  orig home

--- images/drone/v2.22.0-paul-patch-3/custom/home.jsx
+++ images/drone/v2.22.0-paul-patch-3/custom/home.jsx
@@ -3,6 +3,7 @@ import React, {
   useEffect, useState, useMemo, useContext,
 } from 'react';
 
+import Repos from 'components/pages/home/repos';
 import ReposRecent from 'components/pages/home/repos-recent';
 import Button from 'components/shared/button';
 import Input from 'components/shared/form/input';
@@ -19,6 +20,8 @@ import styles from './home.module.scss';
 
 const cx = classNames.bind(styles);
 
+// amount of repos to be shown initially and increased by
+// on show more click
 const REPOS_CHUNK_SIZE = 50;
 
 const RECENT_ACTIVITY = 'Sort by Recent activity';
@@ -27,12 +30,14 @@ const sortEnums = [RECENT_ACTIVITY, NAME];
 
 export default function Home() {
   const [context, setContext] = useContext(AppContext);
-  const [shouldStartSync, setShouldStartSync] = useState(context.isAccSyncing);
+  const [showAllRepos, setShowAllRepos] = useState(false);
   const [isActiveOnly, setIsActiveOnly] = useLocalStorage('home_show_active_only_repos', false);
   const [sortBy, setSortBy] = useLocalStorage('home_sort_repos_by', sortEnums[0]);
   const [filterOrg, setFilterOrg] = useLocalStorage('home_org_repos', '');
+  const [shouldStartSync, setShouldStartSync] = useState(context.isAccSyncing);
   const { showError } = useToast();
   const { hasSyncReqFiredOff, isError: syncError } = useSyncAccount(shouldStartSync);
+
   const { isSynced, isSyncing, isError: viewerError } = useViewer({ withPolling: hasSyncReqFiredOff });
 
   const {
@@ -40,14 +45,29 @@ export default function Home() {
   } = useStore();
   const data = repos ? Object.values(repos) : undefined;
   const isLoading = !data && !error;
-
   useEffect(() => reloadOnce(), [reloadOnce]);
   useCustomTitle();
 
   const [filter, setFilter] = useState('');
 
+  const filtered = useMemo(
+    () => data?.slice(0)
+      .filter((repo) => (isActiveOnly ? repo.active : !!repo))
+      .filter((repo) => (filterOrg ? repo.namespace === filterOrg : !!repo))
+      .filter((item) => item.slug.indexOf(filter) > -1) ?? [],
+    [data, filter, isActiveOnly, filterOrg],
+  );
+
+  const sorted = useMemo(
+    () => filtered
+      .slice(0)
+      .sort(sortBy === NAME ? byRepoNameAsc : byBuildCreatedAtDesc)
+      .slice(0, showAllRepos ? filtered.length : REPOS_CHUNK_SIZE) ?? [],
+    [filtered, showAllRepos, sortBy],
+  );
+
   const recent = useMemo(
-    () => data?.slice(0).sort(byBuildCreatedAtDesc).filter((repo) => !!repo.build) ?? [],
+    () => data?.slice(0).sort(byBuildCreatedAtDesc).filter((repo) => !!repo.build)?.slice(0, 6) ?? [],
     [data],
   );
 
@@ -79,6 +99,59 @@ export default function Home() {
 
   const handleSyncClick = () => setShouldStartSync(true);
 
+  const handleLoadMoreClick = () => setShowAllRepos(true);
+
+  const handleFilter = (e) => setFilter(e.target.value.trim());
+
+  let content = null;
+
+  if (isLoading) {
+    content = null;
+  } else if (data?.length) {
+    content = (
+      <>
+        <div className={cx('subheader')}>
+          <h2 className={cx('section-title')}>Repositories</h2>
+          <div className={cx('actions')}>
+            <Switch
+              id="active-switch"
+              checked={isActiveOnly}
+              onChange={(val) => setIsActiveOnly(val)}
+            >
+              Active Only
+            </Switch>
+            <Select value={sortBy} optionsList={sortOptions} onChange={(e) => setSortBy(e.target.value)} />
+            <Select value={filterOrg} optionsList={orgOptions} onChange={(e) => setFilterOrg(e.target.value)} />
+            <Input
+              placeholder="Filter …"
+              icon="search"
+              className={cx('search')}
+              width={300}
+              name="repo-search"
+              onChange={handleFilter}
+            />
+          </div>
+        </div>
+        <Repos repos={sorted} />
+        {!showAllRepos && sorted.length >= REPOS_CHUNK_SIZE && (
+        <Button
+          className={cx('btn', 'btn-show-more')}
+          onClick={handleLoadMoreClick}
+        >
+          Show more &#8595;
+        </Button>
+        )}
+      </>
+    );
+  } else {
+    content = (
+      <ZeroState
+        title="Your Repository List is Empty."
+        message="Drone automatically syncs with your version control system to display your repositories."
+      />
+    );
+  }
+
   return (
     <>
       <header className={cx('header')}>
@@ -96,17 +169,13 @@ export default function Home() {
         </button>
       </header>
       <section className={cx('wrapper')}>
-        {!!recent.length ? (
-          <>
-            <h2 className={cx('section-title', 'section-title-recent')}>Recent Activity</h2>
-            <ReposRecent repos={recent} />
-          </>
-        ) : !isLoading ? (
-          <ZeroState
-            title="No Recent Builds"
-            message="No repositories with recent builds found."
-          />
-        ) : null}
+        {!!recent.length && (
+        <>
+          <h2 className={cx('section-title', 'section-title-recent')}>Recent Activity</h2>
+          <ReposRecent repos={recent} />
+        </>
+        )}
+        {content}
       </section>
     </>
   );
