10471b4722730dfbf34d37f078b309e552385b04
paulDashkevich <p.dashkevich@uzum.com>
2026-02-24 18:20:17 +0300

    feat: swctl-upd

--- images/strongswan/5.9.14/init.sh
+++ images/strongswan/5.9.14/init.sh
@@ -5,22 +5,6 @@ sysctl -w net.ipv4.ip_forward=1 >/dev/null 2>&1 || true
 sysctl -w net.ipv4.conf.all.rp_filter=0 >/dev/null 2>&1 || true
 sysctl -w net.ipv4.conf.default.rp_filter=0 >/dev/null 2>&1 || true
 
-mkdir -p /var/run
-
-/usr/sbin/charon &
-i=0
-while [ ! -S /var/run/charon.vici ]; do
-  i=$((i + 1))
-  if [ "$i" -gt 100 ]; then
-    echo "charon.vici not found" >&2
-    exit 1
-  fi
-  sleep 0.2
-done
-
-# expects /etc/swanctl/swanctl.conf (+ optionally swanctl.secrets) to be mounted
-swanctl --load-all || true
-
 exec /usr/local/bin/ipsec-exporter \
   -vici-address /var/run/charon.vici \
   -vici-network unix \
